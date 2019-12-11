53430a466079eada157ec2c7f3bb737cd0b65dc3
Nguyen Uy <leesan.itus@gmail.com>
2019-12-11 21:52:30 +0700

    Initial Commit

---
+++ GoogleSignInDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GoogleSignInDemo
+//
+//  Created by Nguyen Uy on 11/12/19.
+//  Copyright © 2019 Nguyen Uy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GoogleSignInDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GoogleSignInDemo
+//
+//  Created by Nguyen Uy on 11/12/19.
+//  Copyright © 2019 Nguyen Uy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
